7c365ad657d0059713139b6b4611422089640529
hjKim-FireCop365 <hjkim@firecop365.com>
2021-01-02 15:43:04 +0900

    push rn second app

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react'
+import {
+  View,
+  StyleSheet,
+  Button,
+  Image,
+  Text,
+  Dimensions,
+  ScrollView,
+} from 'react-native'
+
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
+import MainButton from '../components/MainButton'
+
+import Colors from '../constants/colors'
+
+const GameOverScreen = (props) => {
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState(
+    Dimensions.get('window').width
+  )
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState(
+    Dimensions.get('window').height
+  )
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setAvailableDeviceWidth(Dimensions.get('window').width)
+      setAvailableDeviceHeight(Dimensions.get('window').height)
+    }
+    Dimensions.addEventListener('change', updateLayout)
+
+    return () => {
+      Dimensions.removeEventListener('change', updateLayout)
+    }
+  })
+
+  return (
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View
+          style={{
+            ...styles.imageContainer,
+            ...{
+              width: availableDeviceWidth * 0.7,
+              height: availableDeviceWidth * 0.7,
+              borderRadius: (availableDeviceWidth * 0.7) / 2,
+              marginVertical: availableDeviceHeight / 30,
+            },
+          }}
+        >
+          <Image
+            style={styles.image}
+            source={require('../assets/success.png')}
+            // source={{
+            //   uri:
+            //     "https://images.pexels.com/photos/6945/sunset-summer-golden-hour-paul-filitchkin.jpg?cs=srgb&dl=pexels-snapwire-6945.jpg&fm=jpg",
+            // }}
+            resizeMode="cover"
+            fadeDuration={1000}
+          />
+        </View>
+
+        <View
+          style={{
+            ...styles.resultContainer,
+            ...{
+              marginVertical: availableDeviceHeight / 60,
+            },
+          }}
+        >
+          <BodyText
+            style={{
+              ...styles.resultText,
+              ...{
+                fontSize: availableDeviceHeight < 400 ? 16 : 20,
+              },
+            }}
+          >
+            Your phone needed{' '}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+            guess the number{' '}
+            <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  imageContainer: {
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+  },
+  resultText: {
+    textAlign: 'center',
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold',
+  },
+})
+
+export default GameOverScreen
